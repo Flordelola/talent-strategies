6349869382b095919cb80e0828f5064d01f8157a
flordelola <mhelenaamador@gmail.com>
2025-08-06 16:58:34 +0200

    fix element condition

--- src/app/[lang]/components/heading/heading.tsx
+++ src/app/[lang]/components/heading/heading.tsx
@@ -29,13 +29,13 @@ const textAlignStyle = {
 
 export default function Heading({ data }: { data: Heading}) {
   const {title, subTitle, backgroundColor, titleColor, subTitleColor, button, textAlign} = data
-
+    console.log('button',)
     return (
       <div className={`${"heading-component"} ${backgroundColor ? backgroundColorStyle[backgroundColor] : 'white-bg'}`}>
         <div className={`${"max-container padding-container"} ${textAlign? textAlignStyle[textAlign] : 'left-alignment'}`}>
           {title && (<h1 className={`${"margin-bottom-32"} ${titleColor ? titleColorStyle[titleColor] : 'black-font'}`}>{title}</h1>)}
           {subTitle && (<h2 className={`${"margin-bottom-32"} ${subTitleColor ? subTitleColorStyle[subTitleColor] : 'black-font'}`}>{subTitle}</h2>)}
-          {button[0].label && (
+          {button?.length > 0 && (
             <div className="buttons-container">
               {button.map((item)=>(
                 <Button key={Math.random()} color={item.color} size={item.size} 
